b1c7cee5c1746af509c20dfa09483c4223894c3b
ssanabria <salvadorsanabria14@gmail.com>
2024-05-24 20:18:05 -0500

    api gateway y controllers

--- customer-service/src/main/java/com/taller2/customerservice/controller/CustomerController.java
+++ customer-service/src/main/java/com/taller2/customerservice/controller/CustomerController.java
@@ -1,7 +1,11 @@
 package com.taller2.customerservice.controller;
 
 import com.taller2.customerservice.service.CustomerService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/customer")
 public class CustomerController {
 
     private final CustomerService customerService;
